d0016f24547be4d2367d3be7aeb07cfaff7fd326
Raluca Craciun <ralucacraciun29@gmail.com>
2025-07-08 19:00:56 +0300

    Added new logInTest

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -42,6 +42,22 @@ public class BaseTest {
     }
 
     @Test
+    public void logInTest() {
+        driver.findElement(By.xpath("//a[@id='login2']")).click();
+        Wait.waitInSeconds(1);
+        driver.findElement(By.xpath("//input[@id='loginusername']"))
+                .sendKeys(Constants.USERNAME.getValue());
+        driver.findElement(By.xpath("//input[@id='loginpassword']"))
+                .sendKeys(Constants.PASSWORD.getValue());
+        driver.findElement(By.xpath("//button[@onclick='logIn()']")).click();
+        Wait.waitInSeconds(1);
+        String actualMessage = driver.findElement(By.xpath("//a[@id='nameofuser']")).getText();
+        String expectedMessage = Constants.LOGIN_MESSAGE.getValue();
+        Wait.waitInSeconds(2);
+        Assert.assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
     public void endToEndTest() {
         driver.findElement(By.xpath("//a[@id='login2']")).click();
         Wait.waitInSeconds(1);
